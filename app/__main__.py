392d61da0821bde933afcc4c4e4633af1b0b4006
Tam Nhu Tran <kaitran.ntt@gmail.com>
2024-09-26 19:23:40 -0400

    `src/` to `app/`

---
+++ app/__main__.py
@@ -0,0 +1,7 @@
+# This file allows the application to be run as a module (python -m app)
+# It delegates to the main function in tracker.py
+
+from tracker import main
+
+if __name__ == "__main__":
+    main()
--- app/utils/person_counter.py
+++ app/utils/person_counter.py
@@ -1,5 +1,5 @@
 import time
-from src.utils.logger import get_logger, create_log_message
+from app.utils.logger import get_logger, create_log_message
 
 logger = get_logger(__name__)
 
