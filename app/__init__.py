51ed694f357378503273c66d559b1feab8325e9c
Simon Lévesque <simlevesque@dismail.de>
2025-03-10 10:28:32 -0400

    now it works

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-from .api import start_server
-from .vision import track
-from .utils import list_cameras, PersonCounter
+from .utils import list_cameras
 
 __all__ = ["track", "start_server", "list_cameras", "PersonCounter"]
