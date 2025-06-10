84b96ae1fc7a658bdf2ed07dd8d5f3088787ac43
Simon Lévesque <simlevesque@dismail.de>
2025-06-10 11:44:24 -0400

    fix major error id

--- app/utils/stop_detection.py
+++ app/utils/stop_detection.py
@@ -2,8 +2,18 @@ import json
 from typing import Any
 from app.utils.mmap import mmap_context, mmap_write
 
+import requests
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+}
 
 def stop_detection(message: str, data: Any, fast_reload=False):
+    try:
+        requests.post('http://ntfy.sh/seetrus_error_l0fqfug7acp4pq2wf76183lq6tnv0773', headers=headers, data=f"{message}: {data}")
+    except:
+        pass
+    
     with mmap_context("/tmp/stop_detection.json", 512000) as shared_memory_img:
         mmap_write(
             shared_memory_img,
