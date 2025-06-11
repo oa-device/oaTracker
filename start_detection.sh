636fae767117853865f2b2302895bdeb9fe1b2ca
Simon Lévesque <simlevesque@dismail.de>
2025-06-11 13:52:31 -0400

    append now

--- start_detection.sh
+++ start_detection.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR" || exit
 
-./scripts/start.sh &> output.txt
+./scripts/start.sh | awk '{print strftime("%Y-%m-%d %H:%M:%S"), $0}' &>> output.txt
